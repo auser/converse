5aff335293208f76f4eb2398969d63ad9b6812bf
Ari Lerner <alerner@YPCMC05591.local>
2009-01-29 23:22:52 -0800

    Added pid connection and socket

--- test/example.erl
+++ test/example.erl
@@ -2,12 +2,6 @@
 talker_router:start_link().
 talker_router:get_all_connections().
 talker_router:register_connection({0,0,0,1}, 5001, {name, "fred"}).
-talker_router:register_connection_with_info({0,0,0,2}, 5001, "pid", "socket", {{name, "fred"}}).
-talker_router:register_connection_with_info({0,0,0,3}, 5001, "pid", "socket", {{name, "bob"}}).
-talker_router:register_connection({0,0,0,4}, 5001, "pid", "socket").
-talker_router:get_all_connections().
-talker_router:unregister_connection({0,0,0,2}, 5001).
-talker_router:unregister_connection({0,0,0,1}, 5001).
 talker_router:get_all_connections().
 
 % talker
