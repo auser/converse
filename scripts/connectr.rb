9242ff4478e5ca00a5ec1dc834dd58e7a1707e47
Ari Lerner <alerner@ypcmc05591.yellowpages.local>
2009-02-20 13:39:21 -0800

    Stripping old vsn

--- scripts/connectr.rb
+++
@@ -1,7 +0,0 @@
-require "rubygems"
-require "ruberl"
-
-class TestClass < Ruberl::Base
-end
-
-puts TestClass.new("localhost", 7899).messenger_send!("hi")
--- src/converse_packet.erl
+++
@@ -1,9 +0,0 @@
--module (converse_packet).
-
--export ([encode/1, decode/1]).
-
-encode(Binary) ->
-	term_to_binary(Binary).
-
-decode(Packet) ->
-	binary_to_term(Packet).
