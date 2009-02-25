b039c5de66dd91a7f7f34d0fa8b3a513a7772017
Ari Lerner <alerner@ypcmc05591.yellowpages.local>
2009-02-24 17:23:10 -0800

    Simplifying the hell out of the code

--- src/converse.erl
+++ src/converse.erl
@@ -4,10 +4,11 @@
 -export([send/2,send/3]).
 -export ([start/2]).
 
-start(Type, StartArgs) -> converse_app:start(Type, StartArgs).
+start(Type, StartArgs) -> 
+  converse_app:start(Type, StartArgs).
 
-% send(Addr, Msg) -> send(Addr, Msg, ?DEFAULT_TIMEOUT).
-send(Addr, Msg, Timeout) -> converse_tcp:send(Addr, Msg, Timeout).
+start() -> application:start(?MODULE).
+stop() -> application:stop(?MODULE).
 
-send(Socket, Msg) ->
-	gen_tcp:send(Socket, converse_packet:encode(Msg)).
+start(normal, [Config]) ->
+  [Port] = 
--- src/converse_utils.erl
+++
@@ -1,7 +0,0 @@
--module (converse_utils).
--compile (export_all).
-
-get_registered_name_for_address(Proto, Type, Addr) ->	registered_name(Proto,Type,Addr).
-	
-registered_name(Type, From, Name) when is_atom(Type), is_atom(From), is_list(Name) -> 
-	list_to_atom("converse."++atom_to_list(Type)++"."++atom_to_list(From)++"."++Name).
