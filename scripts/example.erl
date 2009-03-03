df8c94597b7a4ee86985cfb652d4bd676d0780aa
Ari Lerner <alerner@YPCMC05591.local>
2009-03-03 00:30:36 -0800

    Updated with udp instead of tcp

--- scripts/example.erl
+++ scripts/example.erl
@@ -1,33 +1,10 @@
-layers:start([converse, converse_test_app], [{port, 22002}]).
-converse:send_message("0.0.0.0", {data, "TEsting for replies"}).
-converse:send_message("0.0.0.0", {data, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}).
+converse:start(normal, [{port, 9009}]).
+converse:send_message(9009, {ping}).
+converse:send_message("0.0.0.0", 9009, {ping}).
+converse:send_message(9010, {ping}).
 
-converse:start(normal, []).
-converse:send_message("0.0.0.0", "hey").
-converse:send_message("0.0.0.0", 12345).
-converse:cast_message("0.0.0.0", "hey").
+converse:start(normal, [{port, 9010}]).
 
-f().
-{ok, Sock} = gen_tcp:connect({0,0,0,0}, 22002, [binary]).
-gen_tcp:send(Sock, term_to_binary({data, "TEsting for replies"})).
-receive {tcp, S, M} -> M after 1000 -> no_response end.
+converse_udp:send({ping}, "localhost", 9009).
 
-receive
-  {reply, Reply} -> io:format("Received ~p~n", [Reply])
-  after 1000 -> ok
-end.
-
-{ok, Sock} = gen_tcp:connect({10,45,10,228}, 22002, [binary]).
-
-converse:send({0,0,0,0}, {"hi"}).
-
-layers:start([converse, layers_test_app], []).
-converse:send({0,0,0,0}, {"hi"}).
-converse:send({0,0,0,1}, {"hi"}).
-
-converse_udp:start_link(22003).
-converse_udp:send_message({0,0,0,0}, 22003, {"hey"}).
-
-layers:start([converse, converse_test_app], [{port, 22002}]).
-converse:cast_message({0,0,0,0}, {"hey from cast"}).
-converse:send_message({0,0,0,0}, {"hey from send"}).
+converse_udp:send("hi", "localhost", 9009).
--- src/converse.erl
+++ src/converse.erl
@@ -7,24 +7,37 @@
 %% application callbacks
 -export([start/2, stop/1]).
 -export ([init/1]).
--export ([send_message/2, cast_message/2, reply/2]).
--export ([send/2, cast/2]).
+-export ([send_message/2, send_message/3]).
 
-send(Addr, Msg) -> send_message(Addr, Msg).
-send_message(Addr, Msg) -> converse_tcp:send_message(Addr, Msg).
-cast(Addr, Msg) -> cast_message(Addr, Msg).
-cast_message(Addr, Msg) -> converse_tcp:cast_message(Addr, Msg).
+send_message(Port, Msg) -> send_message("0.0.0.0", Port, Msg).
+  
+send_message(Addr, Port, Msg) -> 
+  Socket = converse_udp:send(Msg, Addr, Port),
+  gen_udp:recv(Socket, 4096, 10000),
+  receive
+    {ok, {_,_,Reply}} -> Reply;
+    Error -> no_reply
+  after
+    ?DEFAULT_TIMEOUT ->
+      no_reply
+  end.  
+  
+cast_message(Addr, Msg) -> converse_upd:cast_message(Addr, Msg).
 reply(Socket, Msg) -> converse_tcp:reply_message(Socket, Msg).
 
 start(_Type, Config) ->    
     layers:start_bundle([
       {"Applications", fun() -> [application:start(A) || A <- ?APPLICATIONS_TO_START] end},
-      {"Converse supervisor", fun() -> converse_sup:start_link() end},
-      {"Converse listener", fun() -> supervisor:start_link({local, ?MODULE}, ?MODULE, [Config]) end}
+      {"Converse logger", fun() -> converse_logger:start_link() end},
+      {"Converse listener", fun() -> 
+        [Port] = config:fetch_or_default_config([port], Config, ?DEFAULT_CONFIG),
+        ServerName = converse_utils:registered_name(?MODULE, integer_to_list(Port)),
+        supervisor:start_link(?MODULE, [ServerName, Config]) 
+      end}
     ]).
 
-init([Config]) ->
-  TcpServerSup = { converse_tcp, {converse_tcp,start_link,[Config]}, permanent,2000,worker,[]},
+init([ServerName, Config]) ->
+  TcpServerSup = { converser, {converse_udp,start_named,[ServerName, Config]}, permanent,2000,worker,[]},
   {ok,{_SupFlags = {one_for_one, ?MAXIMUM_RESTARTS, ?MAX_DELAY_TIME},[TcpServerSup]}}.
 
 stop(State) -> ok.
